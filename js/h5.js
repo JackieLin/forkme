28e37b2fa5c3d5f0edbdaf051ae3a88d8b8d67b2
linbin <1028332359@qq.com>
2013-07-09 11:41:54 +0800

    the third commit

---
+++ js/h5.js
@@ -0,0 +1,10 @@
+/**
+ * @author linbin
+ * @content The solve method that IE 6-8 can not support html5 tag
+ */
+document.createElement('header');
+document.createElement('footer');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('nav');
